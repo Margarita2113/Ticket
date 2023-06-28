4e783e422a8f3bec0064dd005275bedca4651788
Oskina Margarita <rita_2113@mail.ru>
2023-06-28 17:21:44 +0300

    Added tests with no one matches and one matches

--- src/test/java/Ticket/Manager/AviaSoulsTest.java
+++ src/test/java/Ticket/Manager/AviaSoulsTest.java
@@ -51,4 +51,46 @@ class AviaSoulsTest {
 
         Assertions.assertArrayEquals(expected, actual);
     }
+
+    @Test
+    public void searchNoTickets() {
+        AviaSouls manager = new AviaSouls();
+        Ticket ticket1 = new Ticket("MOSCOW", "ANAPA", 13_500, 12, 9);
+        Ticket ticket2 = new Ticket("MOSCOW", "BARNAUL", 7_600, 9, 15);
+        Ticket ticket3 = new Ticket("MOSCOW", "ANAPA", 10_000, 10, 14);
+        Ticket ticket4 = new Ticket("MOSCOW", "ANAPA", 11_500, 11, 15);
+        Ticket ticket5 = new Ticket("MOSCOW", "SIMFEROPOL", 15_156, 14, 21);
+        Ticket ticket6 = new Ticket("MOSCOW", "BARNAUL", 5_900, 8, 12);
+        manager.add(ticket1);
+        manager.add(ticket2);
+        manager.add(ticket3);
+        manager.add(ticket4);
+        manager.add(ticket5);
+
+        Ticket[] actual = manager.search("MOSCOW", "SPB");
+        Ticket[] expected = {};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void searchOneTickets() {
+        AviaSouls manager = new AviaSouls();
+        Ticket ticket1 = new Ticket("MOSCOW", "ANAPA", 13_500, 12, 9);
+        Ticket ticket2 = new Ticket("MOSCOW", "BARNAUL", 7_600, 9, 15);
+        Ticket ticket3 = new Ticket("MOSCOW", "ANAPA", 10_000, 10, 14);
+        Ticket ticket4 = new Ticket("MOSCOW", "ANAPA", 11_500, 11, 15);
+        Ticket ticket5 = new Ticket("MOSCOW", "SIMFEROPOL", 15_156, 14, 21);
+        Ticket ticket6 = new Ticket("MOSCOW", "BARNAUL", 5_900, 8, 12);
+        manager.add(ticket1);
+        manager.add(ticket2);
+        manager.add(ticket3);
+        manager.add(ticket4);
+        manager.add(ticket5);
+
+        Ticket[] actual = manager.search("MOSCOW", "SIMFEROPOL");
+        Ticket[] expected = {ticket5};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
